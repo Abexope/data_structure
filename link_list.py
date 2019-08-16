aa923529fdcbb1107c4c0678c782e8685a9d5e3f
Xing Xudong <50110585+abexope@users.noreply.github.com>
2019-08-16 11:07:01 +0800

    1. 添加单项循环链表类CycleLinkList 2. 将类BaseStructure中的batch_process()方法重命名为apply()方法
    Signed-off-by: Xing Xudong <xd_xing@sina.com>

--- link_list.py
+++ link_list.py
@@ -19,6 +19,12 @@ class BaseStructure:
 		
 	def __str__(self):
 		"""自定义链表可视化方法"""
+		
+		# 空表情况
+		if self.is_empty():
+			return "[ -> None]"
+		
+		# 一般情况
 		p = self._head
 		res = ""
 		if self._head is not None:
@@ -138,7 +144,7 @@ class BaseStructure:
 				yield p.elem
 			p = p.next
 	
-	def batch_process(self, fun):
+	def apply(self, fun):
 		"""
 		链表对象批处理方法
 		:param fun:
@@ -310,6 +316,217 @@ class LinkList(BaseStructure):
 			q.next = p
 			p = q           # 将刚刚摘下的节点接入指针p指向的节点序列
 		self._head = p      # 链表反转完成，重置表头指针
+
+
+class CycleLinkList:
+	"""单向循环链表"""
+
+	def __init__(self):
+		self._rear = None
+		self._count = 0
+
+	def __str__(self):
+
+		if self.is_empty():
+			return "[->]"
+
+		p = self._rear.next
+		res = ""
+		if self._rear is not None:
+			while p is not self._rear:
+				res += "->{}".format(p.elem)
+				p = p.next
+			else:
+				"""
+				当p指向self._rear的时候会直接跳出循环
+				因此主循环中无法实现将指针指向最后一个Node，需要添加else语句补充
+				"""
+				res += "->{}->".format(p.elem)
+		return "[{}]".format(res)
+
+	def __len__(self):
+		return self._count
+
+	def __iter__(self):
+		p = self._rear.next
+		while p is not self._rear:
+			yield p.elem
+			p = p.next
+		else:
+			yield p.elem
+
+	def is_empty(self):
+		return self._rear is None
+
+	def prepend(self, elem):
+		"""
+		链表表头插入对象方法
+		:param elem: 待插入对象
+		:return: None
+		"""
+		p = Node(elem=elem)
+		if self._rear is None:  # 空表情况
+			p.next = p          # p.next指针指向p本身，保证循环链表的性质
+			self._rear = p
+			self._count += 1
+		else:                   # 一般情况
+			p.next = self._rear.next
+			self._rear.next = p
+			self._count += 1
+
+	def append(self, elem):
+		"""
+		链表表尾插入对象方法
+		:param elem: 待插入对象
+		:return: None
+		"""
+		p = Node(elem=elem)
+		if self._rear is None:  # 空表情况
+			p.next = p
+			self._rear = p
+			self._count += 1
+		else:                   # 一般情况
+			p.next = self._rear.next
+			self._rear.next = p
+			self._rear = p
+			self._count += 1
+
+	def pop(self, get_elem=False):
+		"""
+		链表表头删除对象方法
+		:param get_elem:
+			·Python.bool    default:False
+			·是（True）否（False）返回已经删除的表尾对象
+		:return: 已经删除的表尾对象
+		"""
+
+		# 空表情况
+		if self._rear is None:
+			raise Underflow("in pop() method: cycle link list object is empty. Nothing can be returned.")
+
+		p = self._rear.next
+
+		if self._rear is p:     # 单个节点情况
+			self._rear = None
+			self._count -= 1
+		else:                   # 一般情况
+			self._rear.next = p.next
+			self._count -= 1
+
+		if get_elem:
+			return p.elem
+		else:
+			return
+
+	def pop_last(self, get_elem=False):
+		"""
+		链表表尾删除对象方法
+		:param get_elem:
+			·Python.bool    default:False
+			·是（True）否（False）返回已经删除的表尾对象
+		:return: 已经删除的表尾对象
+		"""
+
+		# 空表情况
+		if self.is_empty():
+			raise Underflow("in pop_last() method: cycle link list object is empty. Nothing can be returned.")
+
+		e = self._rear.elem
+
+		if self._rear.next is self._rear:   # 单个节点情况
+			self._rear = None
+			self._count -= 1
+			if get_elem:
+				return e
+			else:
+				return
+		else:                               # 一般情况
+			p = self._rear.next
+			while p.next is not self._rear:
+				p = p.next
+			p.next = self._rear.next
+			self._rear = p
+			self._count -= 1
+			return e
+
+	def filter(self, cond):
+		"""
+		链表筛选方法
+			筛选满足指定筛选条件的对象
+		:param cond:
+			·Python.lambda function
+			·筛选条件表达式
+			·例如：cond = lambda x: x > 10
+		:return: 所有满足筛选条件的对象
+		"""
+		p = self._rear.next
+		while p is not self._rear:
+			if cond(p.elem):
+				yield p.elem
+			p = p.next
+
+	def apply(self, fun):
+		"""
+		链表对象批处理方法
+		:param fun:
+			·Python.lambda function，Python.function
+			·批处理方式
+			·例如：fun = lambda x: x ** 2
+		:return:
+		"""
+		p = self._rear.next
+		while p is not self._rear:
+			fun(p.elem)
+			p = p.next
+
+	def insert(self, index, elem):
+		if index > self._count:
+			raise Underflow("in insert() method: index is out of range")
+		if index == 0:
+			self.prepend(elem)
+			return
+		if index == self._count:
+			self.append(elem)
+			return
+		idx, p = 0, self._rear.next
+		while p:
+			if idx + 1 == index:
+				next_p = p.next
+				p.next = Node(elem, next_p)
+				self._count += 1
+				return
+			idx, p = idx + 1, p.next
+
+	def remove(self, index, get_elem=False):
+		"""
+		链表指定位置删除对象方法
+		:param index: 指定位置索引
+			·Python.int
+			·index >= 0 and index <= self._count - 1
+		:param get_elem:
+			·Python.bool    default:False
+			·是（True）否（False）返回已经删除的表尾对象
+		:return: 已经被删除的对象
+		"""
+		if index > self._count:
+			raise Underflow("in remove() method: index is out of range")
+		if index == 0:
+			self.pop(get_elem=get_elem)
+			return
+		if index == self._count:
+			self.pop_last(get_elem=get_elem)
+			return
+		idx, p = 0, self._rear.next
+		while p:
+			if idx + 1 == index:
+				e = p.next
+				p.next = p.next.next
+				self._count -= 1
+				if get_elem:
+					return e
+				else:
+					return
+			idx, p = idx + 1, p.next
 
 
 if __name__ == '__main__':
